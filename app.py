ac03b67890c976842cd6acfec7797158642a0ca4
“SurajsinghBayas” <surajyou24@gmail.com>
2024-10-12 23:30:37 +0530

    updated

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 import json
 import random
 import spacy
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, send_file
 
 app = Flask(__name__)
 
-
- 
 nlp = spacy.load("en_core_web_sm")
-
 
 with open("main.json", "r") as json_file:
     data = json.load(json_file)
 
-
 intents = data["intents"]
-
-
 patterns = []
 labels = []
 for intent in intents:
@@ -25,9 +19,10 @@ for intent in intents:
         patterns.append(pattern.lower())
         labels.append(intent_tag)
 
-@app.route('/')  
+# Serving index.html from the same folder as app.py
+@app.route('/')
 def index():
-    return render_template('index.html')  
+    return send_file('index.html')
 
 @app.route('/chat', methods=['POST'])
 def chat():
@@ -36,17 +31,14 @@ def chat():
     best_match_response = "I'm sorry, I don't understand."
     best_match_score = 0
 
-   
     user_doc = nlp(user_input)
 
-    
     for intent in intents:
         intent_tag = intent["tag"]
         for pattern in intent["patterns"]:
             pattern_doc = nlp(pattern.lower())
-            score = user_doc.similarity(pattern_doc)  
+            score = user_doc.similarity(pattern_doc)
 
-            
             if score > best_match_score:
                 best_match_score = score
                 best_match_response = random.choice(intent["responses"])
